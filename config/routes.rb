20d8c8cf6c711250dcdaeb65001035d333f8e7f9
VSPPedro <pedrovsp109@gmail.com>
2018-01-09 03:35:39 +0000

    changing double quotes to simple quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 require 'api_version_constraint'
 
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json}, constraints: { subdomain: 'api'}, path: "/" do
-    namespace :v1, path: "/", constrains: ApiVersionConstraint.new(version: 1, default: true) do
+  namespace :api, defaults: { format: :json}, constraints: { subdomain: 'api'}, path: '/' do
+    namespace :v1, path: '/', constrains: ApiVersionConstraint.new(version: 1, default: true) do
       resources :users, only: [:show]
     end
   end
