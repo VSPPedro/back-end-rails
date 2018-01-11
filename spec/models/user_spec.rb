819d5ac9805a86a4bf15efe25030a5f51cf4da3c
VSPPedro <pedrovsp109@gmail.com>
2018-01-11 02:46:11 +0000

    Adding user authentication token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe User, type: :model do
       expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: #{Devise.friendly_token}")
     end
   end
+  
+  describe '#generate_authentication_token!' do
+    it 'generates a unique auth token' do
+      allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
+      user.generate_authentication_token!
+      
+      expect(user.auth_token).to eq("#{Devise.friendly_token}")
+    end
+    
+    it 'generates anothe auth token when the current auth token already has been taken' do
+      allow(Devise).to receive(:friendly_token).and_return('abc321xyzTOKEN', 'abc321xyzTOKEN', 'abcXYZ123TOKEN')
+      existing_user = create(:user)
+      user.generate_authentication_token!
+      
+      expect(user.auth_token).not_to eq(existing_user.auth_token) 
+    end
+  end
 end
--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #Start mySQL on Cloud9: mysql-ctl start
 #Execute tests more effectively: bundle exec spring rspec
+#Execute tests from model: bundle exec spring rspec spec/models/user_spec.rb
 #Spec in controller: bundle exec spring rspec spec/requests/api/v1/users_spec.rb 
 #Test with curl: curl http://api.taskmanager.dev:$PORT/users/10000
 rails s -p $PORT -b $IP
