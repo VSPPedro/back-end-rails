927a2720fbeb932f467adce513f231cf25392967
VSPPedro <pedrovsp109@gmail.com>
2018-01-13 05:04:31 +0000

    Adding show action to the tasks controller with tests

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::TasksController < ApplicationController
   before_action :authenticate_with_token!
+  respond_to :json
   
   def index
     tasks = current_user.tasks
     render json: { tasks: tasks }
   end
   
+  def show
+    task = current_user.tasks.find_by(params[:id])
+    respond_with task
+  end
 end
